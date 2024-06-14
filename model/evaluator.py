a352664cb93b09164dbd2ac2fb2891cdb22eec2a
Pierre Hogenhaug <hogenhau@izar1.izar.cluster>
2024-06-14 15:55:09 +0200

    updated evaluator

--- model/evaluator.py
+++ model/evaluator.py
@@ -319,8 +319,15 @@ class QuantizedEvaluator():
         self.policy_model_path = policy_model_path
         self.quantized_model_path = quantized_model_path
 
-        self.quantized_model_args = quantized_model_args
-        self.policy_model_args = policy_model_args
+        if policy_model_args is None:
+            self.policy_model_args = {}
+        else:
+            self.policy_model_args = policy_model_args
+        
+        if quantized_model_args is None:
+            self.quantized_model_args = {}
+        else:
+            self.quantized_model_args = quantized_model_args
 
         self.quantized_dpo_evaluator = DPOModelEvaluator(
             task_type=task_type,
@@ -451,7 +458,7 @@ if __name__ == '__main__':
                     quantized_model_path=quantized_model_path,
                     policy_model_args=dpo_model_args,
                     quantized_model_args=quantized_model_args)
-                orig_size, quantized_size, quantized = evaluator.check_model_quantization()
+                quantized_size, orig_size, quantized = evaluator.check_model_quantization()
                 metrics["orig_model_size"] = orig_size
                 metrics["quantized_model_size"] = quantized_size
                 metrics["quantized"] = quantized
